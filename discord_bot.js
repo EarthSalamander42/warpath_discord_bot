de4b86cbd28444334c7e52bfc95fd7aa63150749
EarthSalamander <jeizyy30@hotmail.fr>
2024-03-07 14:20:13 +0100

    test sending embed message now

--- discord_bot.js
+++ discord_bot.js
@@ -23,18 +23,29 @@ app.use(bodyParser.json());
 
 app.post('/game-start', (req, res) => {
 	const { players, match_id } = req.body;
-
+	const channel = client.channels.cache.get('1215237207292383302'); // Remplace avec l'ID du canal Discord
 	console.log(players);
 	console.log(match_id);
 
-	// Envoie une notification sur le serveur Discord
-	// const channel = client.channels.cache.get('1215237207292383302'); // Remplace avec l'ID du canal Discord
+	// Make a great looking embed showing the 2 players, their steam avatar, their xp, their rank, and their winrate and their donator status
+	if (channel) {
+		const embed = {
+			color: 0x0099ff,
+			title: 'Game started',
+			description: `Match ID: ${match_id}`,
+			fields: players.map((player) => {
+				return {
+					name: player.name,
+					value: "XP: " + player.xp_in_level + "/ " + player.xp_next_level + "\nRank: " + player.rank_title + "\nWinrate: " + player.seasonal_winrate + "" + "\nDonator status: " + player.status,
+					inline: true,
+				};
+			}),
+		};
 
-	// if (channel) {
-	// 	channel.send(`Une nouvelle partie a commencé avec les joueurs suivants : ${players.join(', ')}`);
-	// } else {
-	// 	console.log('Channel not found');
-	// }
+		channel.send({ embeds: [embed] });
+	} else {
+		console.log('Channel not found');
+	}
 
 	res.sendStatus(200);
 });
